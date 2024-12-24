5d23b5fcff173b70e1054031f9b15182d24042d5
Hüseyin Umut Kardaş <kardasumt@gmail.com>
2024-12-24 22:30:23 +0300

    Initial Commit

---
+++ GoogleNews/GoogleNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleNewsApp.swift
+//  GoogleNews
+//
+//  Created by Hüseyin Umut Kardaş on 24.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct GoogleNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
