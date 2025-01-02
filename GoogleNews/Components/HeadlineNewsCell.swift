6a5ecc99eef3f2cd6bc2b5deb0b88e0e59ee72da
Hüseyin Umut Kardaş <kardasumt@gmail.com>
2025-01-02 23:26:29 +0300

    Add HeadlineNewsCell and NewsCell components with basic SwiftUI implementation

---
+++ GoogleNews/Components/HeadlineNewsCell.swift
@@ -0,0 +1,18 @@
+//
+//  HeadlineNewsCell.swift
+//  GoogleNews
+//
+//  Created by Hüseyin Umut Kardaş on 2.01.2025.
+//
+
+import SwiftUI
+
+struct HeadlineNewsCell: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HeadlineNewsCell()
+}
---
+++ GoogleNews/Components/NewsCell.swift
@@ -0,0 +1,18 @@
+//
+//  NewsCell.swift
+//  GoogleNews
+//
+//  Created by Hüseyin Umut Kardaş on 2.01.2025.
+//
+
+import SwiftUI
+
+struct NewsCell: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    NewsCell()
+}
